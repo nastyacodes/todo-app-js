f00f348e7c80a169ea67c4085c91360be0cd9ccf
nastyacodes <70597579+nastyacodes@users.noreply.github.com>
2020-09-03 20:05:09 +0300

    Edit function repaired, code refactoring made

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const todos = [];
+let index;
 
 const btnAdd = document.querySelector('#add-todo');
 const input = document.querySelector('#new-todo');
@@ -13,40 +14,81 @@ const displayTodo = () => {
     const container = document.querySelector('#all-todos');
     container.innerHTML = '';
     for(let todo of todos){
-        const oneTodo = document.createElement('div');
-        oneTodo.classList.add('todo');
-
-        const checkBox = document.createElement('input');
-        checkBox.setAttribute('type', 'checkbox');
-        oneTodo.appendChild(checkBox);
-        checkBox.addEventListener('click', () => {
-            text.classList.toggle('done');
-        });
-
-        const text = document.createElement('input');
-        text.setAttribute('type', 'text');
-        text.setAttribute('value', todo);
-        text.setAttribute('disabled', 'disabled');
-        text.classList.add('todo-input');
-        oneTodo.appendChild(text);
-
-        const editIcon = document.createElement('i');
-        editIcon.classList.add('fa', 'fa-pencil', 'icon', 'fa-2x');
-        oneTodo.appendChild(editIcon);
-        editIcon.addEventListener('click', () => {
-            text.toggleAttribute('disabled');
-        });
-
-        const deleteIcon = document.createElement('i');
-        deleteIcon.classList.add('fa', 'fa-trash', 'icon', 'fa-2x');
-        oneTodo.appendChild(deleteIcon);
-        deleteIcon.addEventListener('click', function () {
-            const input = this.parentElement.querySelector('.todo-input');
-            const index = todos.indexOf(input.value);
-            todos.splice(index, 1);
-            displayTodo();
-        });
-
+        const oneTodo = displayOneTodo(todo);
         container.appendChild(oneTodo);
     }
+}
+
+const displayOneTodo = (todo) => {
+    const oneTodo = document.createElement('div');
+    oneTodo.classList.add('todo');
+
+    const checkBox = createCheckbox();
+    oneTodo.appendChild(checkBox);
+    
+    const textInput = createTextInput(todo);
+    oneTodo.appendChild(textInput);
+
+    checkBox.addEventListener('click', () => {
+        textInput.classList.toggle('done');
+    });
+    
+    const editBtn = createEditBtn(textInput);
+    oneTodo.appendChild(editBtn);
+
+    const deleteBtn = createDeleteDtn();
+    oneTodo.appendChild(deleteBtn);
+
+    return oneTodo;
+};
+
+const createCheckbox = () => {
+    const checkBox = document.createElement('input');
+    checkBox.setAttribute('type', 'checkbox');
+
+    return checkBox;
+}
+
+const createTextInput = (textValue) => {
+    const textInput = document.createElement('input');
+    textInput.setAttribute('type', 'text');
+    textInput.setAttribute('value', textValue);
+    textInput.setAttribute('disabled', 'disabled');
+    textInput.classList.add('todo-input');
+
+    textInput.addEventListener('keyup', function(e) {
+        if(e.keyCode === 13) {
+            textInput.setAttribute('disabled', 'disabled');
+            todos[index] = textInput.value;
+        }
+    });
+
+    return textInput;
+}
+
+const createEditBtn = (textInput) => {
+    const editBtn = document.createElement('i');
+    editBtn.classList.add('fa', 'fa-pencil', 'icon', 'fa-2x');
+
+    editBtn.addEventListener('click', function() {
+        textInput.removeAttribute('disabled');
+        const input = this.parentElement.querySelector('.todo-input');
+        index = todos.indexOf(input.value);
+    });
+
+    return editBtn;
+}
+
+const createDeleteDtn = () => {
+    const deleteBtn = document.createElement('i');
+    deleteBtn.classList.add('fa', 'fa-trash', 'icon', 'fa-2x');
+    
+    deleteBtn.addEventListener('click', function () {
+        const input = this.parentElement.querySelector('.todo-input');
+        const index = todos.indexOf(input.value);
+        todos.splice(index, 1);
+        displayTodo();
+    });
+
+    return deleteBtn;
 }
